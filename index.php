672b7512e0659d411e95619358dee79cb233a4b0
Aziz Ramdan Kurniawan <azizramdan44@gmail.com>
2021-12-24 05:34:00 +0700

    feat: better routing

--- index.php
+++ index.php
@@ -1,15 +1,25 @@
 <?php
 
-function page(string $path) : void
+function page(string $path): void
 {
     include __DIR__.'/pages/'.$path.'.php';
 }
 
-switch ($_SERVER['REQUEST_URI']) {
-    case '/':
+function url(string $path): bool
+{
+    return trim(path(), '/') == trim($path);
+}
+
+function path(): string
+{
+    return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+}
+
+switch (path()) {
+    case url('/'):
         page('index');
         break;
-    case '/endpoint-a':
+    case url('endpoint-a'):
         page('endpoint-a');
         break;
     
